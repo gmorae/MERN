a32604eec8ba2fa58828c5e599188c0cf9505592
gmorae <gmmartins06@gmail.com>
2019-11-08 02:27:19 -0200

    Adicionando  o cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 // Settings
 // Port of server
 app.set('port', process.env.PORT || 12345)
 
 // middlewares
+app.use(cors())
+app.use(express.json())
 
 // routes
 

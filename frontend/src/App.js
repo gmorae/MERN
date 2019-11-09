d9a917efa769ae31923284f87e0d159bd9ab5be5
gmorae <gmmartins06@gmail.com>
2019-11-09 02:33:11 -0200

    Metodo get funcionando

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Navegation from './components/Navgation'
 import CreateUser from './components/CreateUser'
 import UserList from './components/UsersList'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <Router>
       <Navegation />
-
-      <Route path="/" exact component={UserList} />
-      <Route  path="/create" component={CreateUser} />
+      <div className="container p-4">
+        <Route path="/" exact component={UserList} />
+        <Route path="/create" component={CreateUser} />
+      </div>
     </Router>
   );
 }
--- frontend/src/components/UsersList.js
+++ frontend/src/components/UsersList.js
@@ -1,10 +1,58 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 export default class UsersList extends Component {
+
+    state = {
+        users: [],
+        nome: ''
+    }
+
+
+    //Serve para pedir os dados para a API
+    async componentDidMount() {
+        const res = await axios.get('http://localhost:12345/api/users')
+        this.setState({ users: res.data })
+        console.log(this.state.users)
+    }
+
+    onChangeUserName = (e) => {
+        this.setState({
+          nome: e.target.value    
+        })
+    }
+
+    // Metodo que cancela p envio
+    onSubmit = async e => {
+        
+    }
+
+
     render() {
         return (
-            <div>
-                List User
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-4">
+                        <div className="card card-body">
+                            <h3>Create New User</h3>
+                            <form onSubmit={this.onSubmit}>
+                                <div className="from-group">
+                                    <input type="text" className="form-control" onChange={this.onChangeUserName} />
+                                </div>
+                                <button type="submit" className="btn btn-primary">Cadastrar</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div className="col-md-8">
+                        <ul className="list-group">
+                            {
+                                this.state.users.map(user =>
+                                    <li className="list-group-item list-group-item-action" key={user.id_programadores}>
+                                        {user.nome}
+                                    </li>)
+                            }
+                        </ul>
+                    </div>
+                </div>
             </div>
         )
     }
